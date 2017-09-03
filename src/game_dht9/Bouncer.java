c784b17b365fd058c0e1a96ed2353ffdd59aa340
David Tran <dht9@duke.edu>
2017-09-03 02:49:27 -0400

    Added bouncing off sides of paddles and better brick bouncing.

--- src/game_dht9/Bouncer.java
+++ src/game_dht9/Bouncer.java
@@ -16,15 +16,15 @@ public class Bouncer {
 	
     private ImageView myView;
     private Point2D myVelocity;
-    private int BALL_SIZE = 10;
-    public double MAX_BOUNCE_ANGLE = 45;
-    public double BOUNCER_SPEED = 250;
+    private int BALL_SIZE = 15; //30 for symmetry
+    public double MAX_BOUNCE_ANGLE = 60;
+    public double BOUNCER_SPEED = 200;
 
 
     /**
      * Create a bouncer from a given image.
      */
-    public Bouncer (Image image, int screenWidth, int screenHeight) {
+    public Bouncer(Image image, int screenWidth, int screenHeight) {
         myView = new ImageView(image);
         // make sure it stays a circle
         myView.setFitWidth(BALL_SIZE);
@@ -42,7 +42,7 @@ public class Bouncer {
      * 
      * Note, elapsedTime is used to ensure consistent speed across different machines.
      */
-    public void move (double elapsedTime) {
+    public void move(double elapsedTime) {
         myView.setX(myView.getX() + myVelocity.getX() * elapsedTime);
         myView.setY(myView.getY() + myVelocity.getY() * elapsedTime);
     }
@@ -50,7 +50,7 @@ public class Bouncer {
     /**
      * Bounce off the walls represented by the edges of the screen.
      */
-    public void bounce (double screenWidth, double screenHeight) {
+    public void bounce(double screenWidth, double screenHeight) {
         // collide all bouncers against the walls
         if (myView.getX() < 0 || myView.getX() > screenWidth - myView.getBoundsInLocal().getWidth()) {
             myVelocity = new Point2D(-myVelocity.getX(), myVelocity.getY());
@@ -64,35 +64,47 @@ public class Bouncer {
     /**
      * Bounce off the paddle.
      */
-    public void bounceOffPaddle(Paddle myPaddle) { 		
-    		// calculate distance between ball and paddle center
-    		double distFromCenter = myView.getX()+myView.getFitWidth()/2 - (myPaddle.myView.getX() + myPaddle.myView.getFitWidth()/2);
-    		// normalize the distance [-1,1]
-    		double normalizedDistFromCenter = distFromCenter/(myPaddle.myView.getFitWidth()/2);
-    		// calculate angle ball will bounce
-    		double bounceAngle = normalizedDistFromCenter * MAX_BOUNCE_ANGLE;
-    		double ballVx = BOUNCER_SPEED*Math.sin(Math.toRadians(bounceAngle));  
-	    	myVelocity = new Point2D(ballVx, -myVelocity.getY());
+    public void bounceOffPaddle(Paddle myPaddle) { 	
+	    	if (myView.getY() + myView.getFitHeight() <= myPaddle.myView.getY() + myPaddle.myView.getFitHeight()/2
+	    			|| myView.getY() >= myPaddle.myView.getY() + myPaddle.myView.getFitHeight()/2) {
+	    		// calculate distance between ball and paddle center
+	    		double distFromCenter = myView.getX()+myView.getFitWidth()/2 - (myPaddle.myView.getX() + myPaddle.myView.getFitWidth()/2);
+	    		// normalize the distance [-1,1]
+	    		double normalizedDistFromCenter = distFromCenter/(myPaddle.myView.getFitWidth()/2);
+	    		// calculate angle ball will bounce
+	    		double bounceAngle = normalizedDistFromCenter * MAX_BOUNCE_ANGLE;
+	    		double ballVx = BOUNCER_SPEED*Math.sin(Math.toRadians(bounceAngle));  
+		    	myVelocity = new Point2D(ballVx, -myVelocity.getY());
+	    	}
+	    	// check if ball hits left side of paddle
+	    	else if (myView.getX() + myView.getFitWidth()/2 <= myPaddle.myView.getX()) {
+	    		myVelocity = new Point2D((-BOUNCER_SPEED + myPaddle.myVelocity.getX()) * Math.sin(45), BOUNCER_SPEED*Math.cos(45));
+	    		System.out.println((-BOUNCER_SPEED + myPaddle.myVelocity.getX()) * Math.sin(45));
+	    	}
+	    	// check if ball hits right side of paddle
+	    	else if (myView.getX() + myView.getFitWidth()/2 >= myPaddle.myView.getX() + myPaddle.myView.getFitWidth()) {
+	    		myVelocity = new Point2D((BOUNCER_SPEED + myPaddle.myVelocity.getX()) * Math.sin(45), BOUNCER_SPEED*Math.cos(45));
+	    	}
     }
     
     /**
      * Bounce off the bricks.
      */
     public void bounceOffBrick(Brick myBrick) {
-    		// check if ball hit on top of brick
-    		if(myView.getY() + myView.getFitHeight() <= myBrick.getY() + myBrick.getHeight()/4) {
+    		// check if ball hits on top of brick
+    		if(myView.getY() + myView.getFitHeight() <= myBrick.getY() + myBrick.getHeight()/10) {
     			myVelocity = new Point2D(myVelocity.getX(), -myVelocity.getY());
     		}
-    		// check if ball hit on bottom of brick
-    		if(myView.getY() >= myBrick.getY() + myBrick.getHeight()*3/4) {
+    		// check if ball hits on bottom of brick
+    		else if(myView.getY() >= myBrick.getY() + myBrick.getHeight()*9/10) {
     			myVelocity = new Point2D(myVelocity.getX(), -myVelocity.getY());
     		}
-    		// check if ball hit on left of brick
-    		if(myView.getX() + myView.getFitWidth() <= myBrick.getX() + myBrick.getWidth()/4) {
+    		// check if ball hits on left of brick
+    		else if(myView.getX() + myView.getFitWidth() <= myBrick.getX() + myBrick.getWidth()/10) {
     			myVelocity = new Point2D(-myVelocity.getX(), myVelocity.getY());
     		}
-    		// check if ball hit on right of brick
-    		if(myView.getX() >= myBrick.getX() + myBrick.getWidth()*3/4) {
+    		// check if ball hits on right of brick
+    		else if(myView.getX() >= myBrick.getX() + myBrick.getWidth()*9/10) {
     			myVelocity = new Point2D(-myVelocity.getX(), myVelocity.getY());
     		}
     }
