0c2f9bd57808397777c4f2614d9a9600ddc7a73c
David Tran <dht9@duke.edu>
2017-09-07 00:28:40 -0400

    Cleaned up methods

--- src/game_dht9/Paddle.java
+++ src/game_dht9/Paddle.java
@@ -1,70 +1,46 @@
 package game_dht9;
 
 import javafx.geometry.Point2D;
-import javafx.scene.Node;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.shape.Rectangle;
 
-public class Paddle {
-	
+public class Paddle extends Rectangle {
 
 	public static final int PADDLE1_OFFSET = -75;
 	public static final int PADDLE2_OFFSET = 75;
-	private ImageView myView;
-	
+
 	private double PADDLE_HEIGHT = 12;
 	private double PADDLE_WIDTH = 120;
 	private Point2D myVelocity;
-	private double PADDLE_SPEED = 400;
+	private double PADDLE_SPEED = 250;
 	private int isExtended;
-	private Type currentPaddleType;
-	private Type previousPaddleType;
-	
-	enum Type {
+	private PaddleAbility currentPaddleAbility;
+	private PaddleAbility previousPaddleAbility;
+
+	enum PaddleAbility {
 		EXTENDED, STICKY, EDGEWARP, PLAIN;
 	}
-	
+
 	/**
-	 * Create a paddle from a given image.
+	 * Initialize paddle attributes.
 	 */
 	public Paddle(Image image, double y) {
 		// set paddle attributes
-		myView = new ImageView(image);
-		myView.setFitWidth(PADDLE_WIDTH);
-		myView.setFitHeight(PADDLE_HEIGHT);
+		setWidth(PADDLE_WIDTH);
+		setHeight(PADDLE_HEIGHT);
 		// set paddle starting position
-		myView.setX(GameEngine.SCREEN_WIDTH/2 - PADDLE_WIDTH/2);
-		myView.setY(y);
+		setX(GameEngine.SCREEN_WIDTH / 2 - PADDLE_WIDTH / 2);
+		setY(y);
 		myVelocity = new Point2D(0, 0);
 		isExtended = 0;
-		currentPaddleType = Type.PLAIN;
+		currentPaddleAbility = PaddleAbility.PLAIN;
 	}
-	
-	public void chooseAbility(int num) {
-		switch(num) {
-		case 0:
-			this.previousPaddleType = currentPaddleType;
-			this.currentPaddleType = Type.EXTENDED;
-			break;
-		case 1:
-			this.previousPaddleType = currentPaddleType;
-			this.currentPaddleType = Type.STICKY;
-			break;
-		case 2:
-			this.previousPaddleType = currentPaddleType;
-			this.currentPaddleType = Type.EDGEWARP;
-			break;
-		default:
-			this.previousPaddleType = currentPaddleType;
-			this.currentPaddleType = Type.PLAIN;
-			break;
-		}
-	}
-	
 
 	/**
-	 * Give Paddle1 velocity when key pressed.
+	 * 
+	 * Update the movement of the paddles.
+	 * 
 	 */
 	public void startPaddle1(KeyCode code) {
 		// allows paddle1 to move and stop at screen edge
@@ -75,9 +51,6 @@ public class Paddle {
 		}
 	}
 
-	/**
-	 * Give Paddle2 velocity when key pressed.
-	 */
 	public void startPaddle2(KeyCode code) {
 		if (code == KeyCode.D && !isAtRightEdge()) {
 			myVelocity = new Point2D(PADDLE_SPEED, 0);
@@ -85,23 +58,16 @@ public class Paddle {
 			myVelocity = new Point2D(-PADDLE_SPEED, 0);
 		}
 	}
-	
-	/**
-	 * Maintain paddle velocity in frames.
-	 */
+
 	public void move(double elapsedTime) {
 		// if paddle is at edge and is EDGEWARPPED, still enable mobility
-		if(isAtEdge(GameEngine.SCREEN_WIDTH) && currentPaddleType == Type.EDGEWARP) {
-			myView.setX(myView.getX() + myVelocity.getX() * elapsedTime);
-		}
-		else {
-			myView.setX(myView.getX() + myVelocity.getX() * elapsedTime);
+		if (isAtEdge(GameEngine.SCREEN_WIDTH) && currentPaddleAbility == PaddleAbility.EDGEWARP) {
+			this.setX(this.getX() + myVelocity.getX() * elapsedTime);
+		} else {
+			this.setX(this.getX() + myVelocity.getX() * elapsedTime);
 		}
 	}
 
-	/**
-	 * Stop Paddle1 velocity when key released.
-	 */
 	public void stopPaddle1(KeyCode code) {
 		// if RIGHT release while paddle is going right, then stop paddle
 		if (code == KeyCode.RIGHT && myVelocity.getX() == PADDLE_SPEED) {
@@ -111,9 +77,6 @@ public class Paddle {
 		}
 	}
 
-	/**
-	 * Stop Paddle2 velocity when key released.
-	 */
 	public void stopPaddle2(KeyCode code) {
 		// if RIGHT release while paddle is going right, then stop paddle
 		if (code == KeyCode.D && myVelocity.getX() == PADDLE_SPEED) {
@@ -122,100 +85,113 @@ public class Paddle {
 			myVelocity = new Point2D(0, 0);
 		}
 	}
-	
-	/**
-	 * Stop Paddle 1 if it hits the screen edge
-	 */
+
 	public void stopPaddleAtEdge() {
 		// if paddle touches left edge
-		if(myView.getX() <= 0 || myView.getX()+myView.getFitWidth() >= GameEngine.SCREEN_WIDTH)
-			myVelocity = new Point2D(0,0);
+		if (this.getX() <= 0 || this.getX() + this.getWidth() >= GameEngine.SCREEN_WIDTH)
+			myVelocity = new Point2D(0, 0);
 	}
-	
+
+	public void reposition(double x) {
+		this.setX(x);
+		myVelocity = new Point2D(0, 0);
+	}
+
 	/**
-	 * Next methods are paddle ABILITIES
+	 * 
+	 * Initialize and control the paddles' abilities.
+	 *
 	 */
-	public void enablePaddleAbility() {
-		if (this.currentPaddleType == Type.EXTENDED && this.isExtended == 0
-				) {
-			this.doubleExtend();
+	public void chooseAbility(int num) {
+		switch (num) {
+		case 0:
+			this.previousPaddleAbility = currentPaddleAbility;
+			this.currentPaddleAbility = PaddleAbility.EXTENDED;
+			break;
+		case 1:
+			this.previousPaddleAbility = currentPaddleAbility;
+			this.currentPaddleAbility = PaddleAbility.STICKY;
+			break;
+		case 2:
+			this.previousPaddleAbility = currentPaddleAbility;
+			this.currentPaddleAbility = PaddleAbility.EDGEWARP;
+			break;
+		default:
+			this.previousPaddleAbility = currentPaddleAbility;
+			this.currentPaddleAbility = PaddleAbility.PLAIN;
+			break;
 		}
-		else if (this.previousPaddleType == Type.EXTENDED && this.isExtended == 1) {
+	}
+
+	public void enablePaddleAbility() {
+		if (this.currentPaddleAbility == PaddleAbility.EXTENDED && this.isExtended == 0) {
+			this.doubleExtend();
+		} else if (this.previousPaddleAbility == PaddleAbility.EXTENDED && this.isExtended == 1) {
 			// shrink paddle if extended for next level
 			this.doubleShrink();
 		}
 	}
-	public void doubleExtend() {
-		if (isExtended == 1) {
-			doubleShrink();
-		}
-		else {
-			myView.setFitWidth(PADDLE_WIDTH*2);
-			myView.setX(myView.getX()-PADDLE_WIDTH/2);
-			isExtended = 1;
-		}
+
+	public boolean hasAbility(PaddleAbility type) {
+		return type != null && type instanceof PaddleAbility && currentPaddleAbility == type;
 	}
-	public void doubleShrink() {
-		myView.setFitWidth(PADDLE_WIDTH);
-		myView.setX(myView.getX()+PADDLE_WIDTH/2);
-		isExtended = 0;
-	}
+
 	public void edgeWarp() {
-		if(myView.getX() > GameEngine.SCREEN_WIDTH) {
-			myView.setX(0);
-		}
-		else if(myView.getX() + myView.getFitWidth()	< 0) {
-			myView.setX(GameEngine.SCREEN_WIDTH - myView.getFitWidth());
+		if (this.getX() > GameEngine.SCREEN_WIDTH) {
+			this.setX(0);
+		} else if (this.getX() + this.getWidth() < 0) {
+			this.setX(GameEngine.SCREEN_WIDTH - this.getWidth());
 		}
 	}
 
-	
-	
-	public void reposition(double x) {
-		myView.setX(x);
-		myVelocity = new Point2D(0, 0);
+	public void doubleExtend() {
+		if (isExtended == 1) {
+			doubleShrink();
+		} else {
+			this.setWidth(PADDLE_WIDTH * 2);
+			this.setX(this.getX() - PADDLE_WIDTH / 2);
+			isExtended = 1;
+		}
 	}
+
+	public void doubleShrink() {
+		this.setWidth(PADDLE_WIDTH);
+		this.setX(this.getX() + PADDLE_WIDTH / 2);
+		isExtended = 0;
+	}
+
+	/**
+	 * 
+	 * Check if paddle-edge collision
+	 * 
+	 */
+
 	public boolean isAtEdge(double screenWidth) {
-		// check for right screen edge 
 		return (isAtRightEdge() || isAtLeftEdge());
 	}
+
 	public boolean isAtLeftEdge() {
-		// check for left screen edge 
-		return myView.getX() <= 0;
+		return this.getX() <= 0;
 	}
+
 	public boolean isAtRightEdge() {
-		// check for right screen edge 
-		return myView.getX() + myView.getFitWidth() >= GameEngine.SCREEN_WIDTH;
+		return this.getX() + this.getWidth() >= GameEngine.SCREEN_WIDTH;
 	}
-	public double getWidth() {
-		return this.myView.getFitWidth();	
-	}
-	public double getHeight() {
-		return this.myView.getFitHeight();	
-	}
+
+	/**
+	 * 
+	 * Access some attributes of the paddle
+	 * 
+	 */
 	public double getVelocityX() {
 		return myVelocity.getX();
 	}
+
 	public double getVelocityY() {
 		return myVelocity.getY();
 	}
-	public Type getCurrType() {
-		return currentPaddleType;
-	}
-	public boolean isType(Type type) {
-		return type != null && type instanceof Type && currentPaddleType == type;
-	}
-	public double getX() {
-		return this.myView.getX();
-	}
-	public double getY() {
-		return this.myView.getY();
-	}
-	
-	/**
-	 * Returns internal view of bouncer to interact with other JavaFX methods.
-	 */
-	public Node getView() {
-		return myView;
+
+	public PaddleAbility getCurrPaddleAbility() {
+		return currentPaddleAbility;
 	}
 }
