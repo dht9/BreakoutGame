b177a81379678b638e273526b33a7371ff8237b2
David Tran <dht9@duke.edu>
2017-09-07 00:28:51 -0400

    Added all power up bricks

--- src/game_dht9/Brick.java
+++ src/game_dht9/Brick.java
@@ -5,15 +5,16 @@ import javafx.scene.shape.Rectangle;
 
 public class Brick extends Rectangle {
 
-	public static final int BRICK_WIDTH = 50;
-	public static final int BRICK_HEIGHT = 25;
+	public static final double BRICK_WIDTH = 47.5;
+	public static final double BRICK_HEIGHT = 25;
 	public static final int BRICK_GAP = 5;
 	
 	private BrickType brickType;
 
 	enum BrickType {
-		LIFE(11, Color.GREEN), BARRIER(10, Color.GRAY), INFINITE(8, Color.GRAY), HIGH(3, Color.web("#FF007F")), MEDIUM(2, Color.web("#FF66B2")), LOW(1,
-				Color.web("#FFCCE5")), DESTROYED(0, Color.WHITE);
+		BARRIER(10, Color.GRAY), CREATE_BARRIER(9, Color.WHITE), INFINITE(8, Color.GRAY), LIFE(7, Color.web("#32cd32")), 
+		EXPAND_BOUNCER(6, Color.YELLOW), SLOW_BOUNCER(5, Color.RED),
+		HIGH(3, Color.web("#FF007F")), MEDIUM(2, Color.web("#FF66B2")), LOW(1,Color.web("#FFCCE5")), DESTROYED(0, Color.WHITE);
 		private int health;
 		private Color color;
 
@@ -59,14 +60,23 @@ public class Brick extends Rectangle {
 
 	public void setBrickType(int brickNum) {
 		switch (brickNum) {
-		case 11:
-			this.brickType = BrickType.LIFE;
-			break;
 		case 10:
 			this.brickType = BrickType.BARRIER;
 			break;
+		case 9:
+			this.brickType = BrickType.CREATE_BARRIER;
+			break;
 		case 8:
 			this.brickType = BrickType.INFINITE;
+			break;
+		case 7:
+			this.brickType = BrickType.LIFE;
+			break;
+		case 6:
+			this.brickType = BrickType.EXPAND_BOUNCER;
+			break;
+		case 5:
+			this.brickType = BrickType.SLOW_BOUNCER;
 			break;
 		case 3:
 			this.brickType = BrickType.HIGH;
@@ -86,13 +96,25 @@ public class Brick extends Rectangle {
 
 	public void decrementType() {
 		switch (this.brickType.health) {
-		case 11:
+		case 10:
+			break;
+		case 9:
 			brickType = BrickType.DESTROYED;
 			this.destroyBrick();
 			break;
-		case 10:
-			break;
 		case 8:
+			break;
+		case 7:
+			brickType = BrickType.DESTROYED;
+			this.destroyBrick();
+			break;
+		case 6:
+			brickType = BrickType.DESTROYED;
+			this.destroyBrick();
+			break;
+		case 5:
+			brickType = BrickType.DESTROYED;
+			this.destroyBrick();
 			break;
 		case 3:
 			this.brickType = BrickType.MEDIUM;
