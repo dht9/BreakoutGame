936d9227680d25a1774ef070e7f4bdd42aad18c3
David Tran <dht9@duke.edu>
2017-09-08 21:41:45 -0400

    removed an import

--- src/game_dht9/StartMenu.java
+++ src/game_dht9/StartMenu.java
@@ -1,13 +1,11 @@
 package game_dht9;
 
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 
 public class StartMenu extends VBox {
 	
