0cbc9734e7ad0c59df9f9c71223ca7187827d1e4
David Tran <dht9@duke.edu>
2017-09-12 22:10:11 -0400

    Added comments and slight modifications for Sprint 3 analysis (code masterpiece)

--- src/game_dht9/HeadsUpDisplay.java
+++ src/game_dht9/HeadsUpDisplay.java
@@ -10,7 +10,44 @@ import javafx.scene.paint.Color;
 /**
  * HeadsUpDisplay (HUD) class used in Breakout Game.
  * 
- * Purpose: to add game updates/statuses to the screen during gameplay.
+ * Purpose: to define a status display to be implemented in the Breakout
+ * gameplay screen.
+ * 
+ * Design Justification for Sprint 3 Analysis:
+ * 
+ * This class is what I regard as a well designed class due to the relationship
+ * between its purpose and size and how it is implemented. As discussed in
+ * Analysis.md, this class was created from the refactoring of code from
+ * GameEngine.java which defined as well as implemented the status display for
+ * the game, all in one class. Learning from refactoring methods discussed in
+ * class, such as with lab_hangman and example_bins, and reading the design
+ * checklist that each class should have a small, well-defined purpose
+ * (modularity), I made the design change that the status display should be
+ * defined in its own class (HeadsUpDisplay.java). Since this class extends
+ * BorderPane, I learned that I could create a HeadsUpDisplay object and call methods
+ * on that instance in order to create HBox's and labels to be aligned on that
+ * instance.
+ * 
+ * This design is flexible in one way because the constructor uses public
+ * constants to set the size of the HeadsUpDisplay. Thus, if the screen width and
+ * height or the brick height changes in GameEngine.java/Brick.java, the new
+ * size of the HeadsUpDisplay would would automatically be adapted.
+ * 
+ * This design is flexible in another way given by the parameters of
+ * createHUDLabel. Initially, I had two almost identical methods, each handling
+ * the case where 'Object value' was either an IntegerProperty or
+ * StringProperty. At first, I did not think I could combine the two methods
+ * because 'value' was of a different type for each method. However, after
+ * seeing the equals method in Disk.java from lab_bins
+ * (https://coursework.cs.duke.edu/CompSci308_2017Spring/lab_bins/blob/master/src/Disk.java),
+ * I noticed that I could use an 'Object value' parament and use an 'if'
+ * statement to check for what the object is an instance of and make
+ * corresponding changes. This effectively reduced two nearly identical methods
+ * into one.
+ * 
+ * Shown from the previous points made, I think this class encapsulates a
+ * substantial portion of what I have learned so far in CS 308 regarding project
+ * design.
  * 
  * @author David Tran (dht9)
  */
@@ -22,28 +59,29 @@ public class HeadsUpDisplay extends BorderPane {
 	}
 
 	/**
-	 * Initialize game status labels during gameplay.
+	 * Initialize game status display with HBox and Label objects.
 	 * 
 	 * @param description
-	 *            non-dynamic label to describe the dynamic label
+	 *            non-dynamic label, describes the parameter 'value'
 	 * @param value
 	 *            dynamic number or string displayed
 	 * @param position
-	 *            define label location on the screen
+	 *            location of the HBox on the HeadsUpDisplay
 	 */
 	public void createHUDLabel(String description, Object value, String position) {
 		HBox hbox = new HBox();
 		Label label = new Label(description);
-		label.setTextFill(Color.WHITE);
 		Label val = new Label();
+
 		if (value != null && value instanceof IntegerProperty) {
 			val.textProperty().bind(((IntegerProperty) value).asString());
-			hbox.getChildren().addAll(label, val);
 		} else if (value != null && value instanceof StringProperty) {
 			val.textProperty().bind(((StringProperty) value));
-			hbox.getChildren().addAll(label, val);
 		}
+
+		label.setTextFill(Color.WHITE);
 		val.setTextFill(Color.WHITE);
+		hbox.getChildren().addAll(label, val);
 
 		if (position.equalsIgnoreCase("left"))
 			this.setLeft(hbox);
